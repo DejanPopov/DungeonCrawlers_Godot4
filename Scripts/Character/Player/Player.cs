9c2447ee1bb90444089fc9c58d3c81212d9cbff3
Dejan Popov <dekipopica92@gmail.com>
2024-05-16 17:00:27 +0200

    Player input and physics loging
    - Added two methods for capturing player input and for player physics process that will happen every second of the frame

--- Scripts/Character/Player/Player.cs
+++ Scripts/Character/Player/Player.cs
@@ -3,4 +3,13 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    public override void _PhysicsProcess(double delta)
+    {   
+        GD.Print("Player Physics Process!");
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        GD.Print("Player Input");
+    }
 }
