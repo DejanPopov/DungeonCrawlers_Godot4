a22e9ab5fe8f37f7bf90868e56bb35fecd3e585a
Dejan Popov <dekipopica92@gmail.com>
2024-05-20 17:43:14 +0200

    State machine - IDLE and MOVING
    - Adding nodes attached to Player node for different states and script for IDLE state.

--- Scripts/Character/Player/Player.cs
+++ Scripts/Character/Player/Player.cs
@@ -4,15 +4,10 @@ using System;
 public partial class Player : CharacterBody3D
 {
     [ExportGroup("Required Nodes")]
-    [Export] private AnimationPlayer animPlayerNode;
-    [Export] private Sprite3D spriteNode;
+    [Export] public AnimationPlayer animPlayerNode;
+    [Export] public Sprite3D spriteNode;
 
     private Vector2 direction = new();
-
-    public override void _Ready()
-    {
-        animPlayerNode.Play(GameConstants.ANIM_IDLE);
-    }
 
     public override void _PhysicsProcess(double delta)
     {   
@@ -33,15 +28,6 @@ public partial class Player : CharacterBody3D
             GameConstants.INPUT_MOVE_BACKWARD,
             GameConstants.INPUT_MOVE_FORWARD
         );
-
-        if (direction == Vector2.Zero)
-        {
-            animPlayerNode.Play(GameConstants.ANIM_IDLE);
-        }
-        else
-        {
-            animPlayerNode.Play(GameConstants.ANIM_MOVE);
-        }
     }
 
     private void Flip()
