c8ce978920adfd6bf89d0c75ca8c460bd91d4d9d
Dejan Popov <dekipopica92@gmail.com>
2024-05-24 17:10:29 +0200

    Update PlayerDashState.cs
    - Updating dash state correctly

--- Scripts/Character/Player/PlayerDashState.cs
+++ Scripts/Character/Player/PlayerDashState.cs
@@ -1,10 +1,13 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class PlayerDashState : Node
 {
     private Player characterNode;
+
     [Export] private Timer dashTimerNode;
+    [Export] private float speed = 10;
 
     public override void _Ready()
     {
@@ -19,12 +22,30 @@ public partial class PlayerDashState : Node
         if (what == 5001)
         {
             characterNode.animPlayerNode.Play(GameConstants.ANIM_DASH);
+            characterNode.Velocity = new(characterNode.direction.X,0,characterNode.direction.Y);
+
+            if (characterNode.Velocity == Vector3.Zero)
+            {
+                characterNode.Velocity = characterNode.spriteNode.FlipH ?
+                Vector3.Left :
+                Vector3.Right;
+            }
+
+            characterNode.Velocity *= speed;
             dashTimerNode.Start();
         }
+
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        characterNode.MoveAndSlide();
+        characterNode.Flip();
     }
 
     private void HandleDashTimeout()
     {
-            characterNode.stateMachineNode.SwitchState<PlayerIdleState>();
+        characterNode.Velocity = Vector3.Zero;
+        characterNode.stateMachineNode.SwitchState<PlayerIdleState>();
     }
 }
