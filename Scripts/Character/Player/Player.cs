07718fb2e4ece03c9c9498ad8dcf2b56aafdcf53
Dejan Popov <dekipopica92@gmail.com>
2024-06-04 23:44:03 +0200

    Update Player.cs
    - Removed code

--- Scripts/Character/Player/Player.cs
+++ Scripts/Character/Player/Player.cs
@@ -1,14 +1,8 @@
 using Godot;
 using System;
 
-public partial class Player : CharacterBody3D
+public partial class Player : Character
 {
-    [ExportGroup("Required Nodes")]
-    [Export] public AnimationPlayer AnimPlayerNode   {get; private set;}
-    [Export] public Sprite3D        SpriteNode       {get; private set;}
-    [Export] public StateMachine    StateMachineNode {get; private set;}
-
-
     public override void _Input(InputEvent @event)
     {   
         direction = Input.GetVector
