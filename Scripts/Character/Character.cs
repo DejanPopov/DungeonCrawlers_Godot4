5e597bc8692ba3a38821231a0b609ded08513789
Dejan Popov <dekipopica92@gmail.com>
2024-06-04 23:42:21 +0200

    Create Character.cs
    - Created Character class

---
+++ Scripts/Character/Character.cs
@@ -0,0 +1,25 @@
+using Godot;
+
+public abstract partial class Character : CharacterBody3D
+{
+    [ExportGroup("Required Nodes")]
+    [Export] public AnimationPlayer AnimPlayerNode   {get; private set;}
+    [Export] public Sprite3D        SpriteNode       {get; private set;}
+    [Export] public StateMachine    StateMachineNode {get; private set;}
+
+    public Vector2 direction = new();
+
+    public void Flip()
+    {  
+        bool isMovingHorizontally = Velocity.X == 0;
+
+        if (isMovingHorizontally)
+        {
+            return;
+        }
+
+        bool isMovingLeft = Velocity.X < 0;
+        SpriteNode.FlipH = isMovingLeft;
+    }
+
+}
