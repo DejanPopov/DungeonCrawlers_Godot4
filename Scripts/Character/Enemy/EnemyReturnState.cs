e2243c86488ec5522ced1ba0a1e5f295aa9f7717
dejan.popov@securitasdirect.es <dekipopica92@gmail.com>
2024-06-05 12:44:11 +0200

    Get true global position of enemy

--- Scripts/Character/Enemy/EnemyReturnState.cs
+++ Scripts/Character/Enemy/EnemyReturnState.cs
@@ -9,11 +9,16 @@ public partial class EnemyReturnState : EnemyState
     {
         base._Ready();
 
-        destination = characterNode.PathNode.Curve.GetPointPosition();
+        Vector3 localPos  = characterNode.PathNode.Curve.GetPointPosition(0);
+        Vector3 globasPos = characterNode.PathNode.GlobalPosition;
+        destination = localPos + globasPos;
     }
 
     protected override void EnterState()
     {
         characterNode.AnimPlayerNode.Play(GameConstants.ANIM_MOVE);
+
+
+        characterNode.GlobalPosition = destination;
     }
 }
