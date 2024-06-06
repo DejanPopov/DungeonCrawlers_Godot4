63300255eaaadab7fae026b134e48bf3f9cd34ac
dejan.popov@securitasdirect.es <dekipopica92@gmail.com>
2024-06-06 13:32:38 +0200

    Update EnemyPatrolState.cs
    - enemy should do a full patrol

--- Scripts/Character/Enemy/EnemyPatrolState.cs
+++ Scripts/Character/Enemy/EnemyPatrolState.cs
@@ -3,16 +3,28 @@ using System;
 
 public partial class EnemyPatrolState : EnemyState
 {
+    private int pointIndex = 0;
+
     protected override void EnterState()
     {
         characterNode.AnimPlayerNode.Play(GameConstants.ANIM_MOVE);
-
-        destination = GetPointGlobalPosition(1);
+        pointIndex = 1;
+        destination = GetPointGlobalPosition(pointIndex);
         characterNode.AgentNode.TargetPosition = destination;
+
+        characterNode.AgentNode.NavigationFinished += HandleNavigationFinished;
+        
     }
 
     public override void _PhysicsProcess(double delta)
     {
         Move();
+    }
+
+    private void HandleNavigationFinished()
+    {
+        pointIndex = Mathf.Wrap(pointIndex + 1,0,characterNode.PathNode.Curve.PointCount);
+        destination = GetPointGlobalPosition(pointIndex);
+        characterNode.AgentNode.TargetPosition = destination;
     }
 }
