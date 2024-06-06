d811bdb37fe75fe8e609dbc2bf7649c6b430efa4
dejan.popov@securitasdirect.es <dekipopica92@gmail.com>
2024-06-06 13:48:02 +0200

    Update EnemyPatrolState.cs
    - random pausing on patrols

--- Scripts/Character/Enemy/EnemyPatrolState.cs
+++ Scripts/Character/Enemy/EnemyPatrolState.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class EnemyPatrolState : EnemyState
 {
+    [Export] private Timer idleTimerNode;
+    [Export(PropertyHint.Range, "0,20,0.1")] private float maxIdleTime = 4;
     private int pointIndex = 0;
 
     protected override void EnterState()
@@ -14,15 +16,31 @@ public partial class EnemyPatrolState : EnemyState
 
         characterNode.AgentNode.NavigationFinished += HandleNavigationFinished;
         
+        idleTimerNode.Timeout += HandleTimeout;
     }
 
     public override void _PhysicsProcess(double delta)
     {
+        if (!idleTimerNode.IsStopped())
+        {
+            return;
+        }
+        
         Move();
     }
 
     private void HandleNavigationFinished()
     {
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_IDLE);
+
+        RandomNumberGenerator rng = new();
+        idleTimerNode.WaitTime = rng.RandfRange(0,maxIdleTime);
+        idleTimerNode.Start();
+    }
+
+    private void HandleTimeout()
+    {
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_MOVE);
         pointIndex = Mathf.Wrap(pointIndex + 1,0,characterNode.PathNode.Curve.PointCount);
         destination = GetPointGlobalPosition(pointIndex);
         characterNode.AgentNode.TargetPosition = destination;
