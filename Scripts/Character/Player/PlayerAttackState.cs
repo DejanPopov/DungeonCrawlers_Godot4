84b3ca2f213c9773a462d6912ce481eb3fc6b357
dejan.popov@securitasdirect.es <dekipopica92@gmail.com>
2024-06-10 11:23:36 +0200

    Player attacking with combo of kicking and slashing

--- Scripts/Character/Player/PlayerAttackState.cs
+++ Scripts/Character/Player/PlayerAttackState.cs
@@ -3,4 +3,28 @@ using System;
 
 public partial class PlayerAttackState : PlayerState
 {
+    private int comboCounter  = 1;
+    private int maxComboCount = 2;
+
+    protected override void EnterState()
+    {
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_ATTACK + comboCounter);
+
+        characterNode.AnimPlayerNode.AnimationFinished += HandleAnimationFinished;
+    }
+
+    protected override void ExitState()
+    {
+        characterNode.AnimPlayerNode.AnimationFinished -= HandleAnimationFinished;
+    }
+
+    private void HandleAnimationFinished(StringName animName)
+    {
+        comboCounter++;
+
+        comboCounter = Mathf.Wrap(comboCounter,1,maxComboCount + 1);
+
+        characterNode.StateMachineNode.SwitchState<PlayerIdleState>();
+
+    }
 }
