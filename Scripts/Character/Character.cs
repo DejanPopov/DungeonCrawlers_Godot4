e36d3158abb5a6a54c1531c911adb31465d49c94
dejan.popov@securitasdirect.es <dekipopica92@gmail.com>
2024-06-11 11:36:55 +0200

    Subscription to Hurtbox signal

--- Scripts/Character/Character.cs
+++ Scripts/Character/Character.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 public abstract partial class Character : CharacterBody3D
@@ -6,6 +7,7 @@ public abstract partial class Character : CharacterBody3D
     [Export] public AnimationPlayer AnimPlayerNode   {get; private set;}
     [Export] public Sprite3D        SpriteNode       {get; private set;}
     [Export] public StateMachine    StateMachineNode {get; private set;}
+    [Export] public Area3D          HurtBoxNode      {get; private set;}
 
     [ExportGroup("AI Nodes")]
     [Export] public NavigationAgent3D AgentNode      {get; private set;} 
@@ -14,6 +16,16 @@ public abstract partial class Character : CharacterBody3D
     [Export] public Area3D            AttackAreaNode {get; private set;}
 
     public Vector2 direction = new();
+
+    public override void _Ready()
+    {
+        HurtBoxNode.AreaEntered += HandleHurtboxEntered;
+    }
+
+    private void HandleHurtboxEntered(Area3D area)
+    {
+        GD.Print($"{area.Name} hit");
+    }
 
     public void Flip()
     {  
