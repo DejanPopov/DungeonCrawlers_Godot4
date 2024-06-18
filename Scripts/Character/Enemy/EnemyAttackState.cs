e7ff0e5393d5744ba4100a5a29320050127e0ed1
dejan.popov@securitasdirect.es <dekipopica92@gmail.com>
2024-06-18 12:01:53 +0200

    Enemy hitbox appears only when attack animation is ongoing

--- Scripts/Character/Enemy/EnemyAttackState.cs
+++ Scripts/Character/Enemy/EnemyAttackState.cs
@@ -1,11 +1,24 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class EnemyAttackState : EnemyState
 {
+    private Vector3 targetPosition;
+
     protected override void EnterState()
     {
         characterNode.AnimPlayerNode.Play(GameConstants.ANIM_ATTACK);
+
+        Node3D target = characterNode.AttackAreaNode.GetOverlappingBodies().First();
+
+        targetPosition = target.GlobalPosition;
+    }
+
+    private void PerformHit()
+    {
+        characterNode.ToggleHitbox(false);
+        characterNode.HitBoxNode.GlobalPosition = targetPosition;
     }
 
 }
