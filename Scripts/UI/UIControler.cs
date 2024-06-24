a09645d4a76d6b4bee008663e6e3679cd9aa7cf1
dejan.popov@securitasdirect.es <dekipopica92@gmail.com>
2024-06-24 14:27:13 +0200

    Dictionary for UI elements

---
+++ Scripts/UI/UIControler.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq
+
+public partial class UIControler : Control
+{
+    private Dictionary<ContainerType,UIContainer> containers;
+
+    public override void _Ready()
+    {
+        containers = GetChildren().Where((element) => element is UIContainer).Cast<UIContainer>().ToDictionary((element) => element.container);
+    }
+}
