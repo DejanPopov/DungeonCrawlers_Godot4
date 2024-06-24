1f9c1acccb05c653e5b80b298e311059acce5e1d
dejan.popov@securitasdirect.es <dekipopica92@gmail.com>
2024-06-24 14:39:25 +0200

    Pausing game until pressed Start Game button

--- Scripts/UI/UIContainer.cs
+++ Scripts/UI/UIContainer.cs
@@ -3,5 +3,6 @@ using System;
 
 public partial class UIContainer : VBoxContainer
 {
-    [Export] public ContainerType container {get; private set;}
+    [Export] public ContainerType container  {get; private set;}
+    [Export] public Button        ButtonNode {get; private set;}
 }
--- Scripts/UI/UIControler.cs
+++ Scripts/UI/UIControler.cs
@@ -12,5 +12,14 @@ public partial class UIControler : Control
         containers = GetChildren().Where((element) => element is UIContainer).Cast<UIContainer>().ToDictionary((element) => element.container);
 
         containers[ContainerType.Start].Visible = true;
+
+        containers[ContainerType.Start].ButtonNode.Pressed += HandleStartPressed;
+    }
+
+    private void HandleStartPressed()
+    {
+        GetTree().Paused = false;
+
+        containers[ContainerType.Start].Visible = false;
     }
 }
