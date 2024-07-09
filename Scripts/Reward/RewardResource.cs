35090ba9f6a9b4d4da1faeb7e01160b75fc99a0f
dejan.popov@securitasdirect.es <dekipopica92@gmail.com>
2024-07-09 16:56:30 +0200

    Update

--- Scripts/Reward/RewardResource.cs
+++ Scripts/Reward/RewardResource.cs
@@ -6,6 +6,7 @@ public partial class RewardResource : Resource
     [Export] public Texture2D SpriteTexture {get; private set;}
     [Export] public string    Description   {get; private set;}
     [Export] public Stat      TargetStat    {get; private set;}
-    [Export(PropertyHint.Range, "1,100,1")] public float    Amount         {get; private set;}
+    [Export(PropertyHint.Range, "1,100,1")] 
+    public float Amount {get; private set;}
 
 }
