02c2a768cc18b37aab107ea1c9911ce832fa1abe
dejan.popov@securitasdirect.es <dekipopica92@gmail.com>
2024-07-12 12:16:48 +0200

    Bomb expand and explode logic

--- Scripts/Abilities/Bomb.cs
+++ Scripts/Abilities/Bomb.cs
@@ -3,4 +3,20 @@ using System;
 
 public partial class Bomb : Node3D
 {
+    [Export] private AnimationPlayer playerNode;
+
+    public override void _Ready()
+    {
+        playerNode.AnimationFinished += HandleExpandAnimationFinished;
+    }
+
+    private void HandleExpandAnimationFinished(StringName animName)
+    {
+        if (animName == GameConstants.ANIM_EXPAND) 
+        {
+            playerNode.Play(GameConstants.ANIM_EXPLOSION);
+        }
+    }
 }
+
+
--- Scripts/General/GameCostants.cs
+++ Scripts/General/GameCostants.cs
@@ -6,6 +6,8 @@ public class GameConstants
     public const string ANIM_DASH             = "Dash";
     public const string ANIM_ATTACK           = "Attack";
     public const string ANIM_DEATH            = "Death";
+    public const string ANIM_EXPAND           = "Expand";
+    public const string ANIM_EXPLOSION        = "Explosion";
     
     // Inputs 
     public const string INPUT_MOVE_LEFT       = "MoveLeft";
