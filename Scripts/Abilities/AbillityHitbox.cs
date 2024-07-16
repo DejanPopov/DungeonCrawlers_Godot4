751f41e1f949c4e965b36b5ff369b87945ef877b
dejan.popov@securitasdirect.es <dekipopica92@gmail.com>
2024-07-16 12:29:29 +0200

    Adding stun function

--- Scripts/Abilities/AbillityHitbox.cs
+++ Scripts/Abilities/AbillityHitbox.cs
@@ -3,5 +3,10 @@ using System;
 
 public partial class AbillityHitbox : Area3D, IHitbox
 {
+    public bool CanStun()
+    {
+        return true;
+    }
+
     public float GetDamage() => GetOwner<Ability>().Damage;
 }
--- Scripts/Character/AttackHitbox.cs
+++ Scripts/Character/AttackHitbox.cs
@@ -3,6 +3,11 @@ using System;
 
 public partial class AttackHitbox : Area3D, IHitbox
 {
+    public bool CanStun()
+    {
+        return false;
+    }
+
     public float GetDamage()
     {
         return GetOwner<Character>().GetStatResource(Stat.Strength).StatValue;
